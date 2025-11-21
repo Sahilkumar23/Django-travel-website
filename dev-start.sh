825d93f35ee8fa05791beb8adb327f679f0048fa
Sahil Kumar <letsmail.sahilkumar@gmail.com>
2025-11-21 21:10:26 +0530

    committed

--- dev-start.sh
+++ dev-start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-# dev-start.sh - start Django dev server in a tmux session
 APP_DIR="/home/sahil/travel-website"
 VENV="$APP_DIR/venv"
 MANAGE="$APP_DIR/manage.py"
 HOST="0.0.0.0"
 PORT="8000"
 SESSION="travel-app"
+LOG="$APP_DIR/server.log"
 
-# Activate venv and start server in detached tmux session
-# If session exists, just print a message
 tmux has-session -t $SESSION 2>/dev/null
 if [ $? -eq 0 ]; then
   echo "tmux session $SESSION already running"
   exit 0
 fi
 
-# start the session with the server command
-tmux new -d -s $SESSION "cd $APP_DIR && $VENV/bin/python $MANAGE runserver $HOST:$PORT"
-echo "Started tmux session: $SESSION"
+tmux new -d -s $SESSION "cd $APP_DIR && $VENV/bin/python $MANAGE runserver $HOST:$PORT > $LOG 2>&1"
+echo "Started tmux session: $SESSION (logging to $LOG)"
